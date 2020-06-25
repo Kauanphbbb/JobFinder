09a67071a8e40ad5df73c5ca2f35e3ebc774ed27
Kauanphbbb <kauanphbbb@gmail.com>
2020-06-25 14:59:01 -0300

    Criando conexão com o banco e modelo de JOB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./DB/connection");
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,17 @@ app.listen(PORT, () => {
     console.log("Rodando na porta 3000");
 });
 
+//DB connection
+db
+.authenticate()
+.then(() => {
+    console.log("Conectou com sucesso")
+})
+.catch(err => {
+    console.log("Ocorreu um erro ", err)
+});
+
+//Routes
 app.get("/", (req, res) => {
     res.send("Tudo funcionando 2");
 });
