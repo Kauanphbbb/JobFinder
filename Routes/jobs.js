18c73fcc7e79c6c5d7c69aa676b874fba2287b6e
Kauanphbbb <kauanphbbb@gmail.com>
2020-06-26 18:26:14 -0300

    Projeto finalizado

--- Routes/jobs.js
+++ Routes/jobs.js
@@ -4,12 +4,27 @@ const Job = require("../Models/Job");
 
 //Rota de teste
 router.get("/test", (req, res) => {
-    res.send("Deu certo");
+  res.send("Deu certo");
 });
 
+//form da rota de envio
 router.get("/add", (req, res) => {
   res.render("add");
 });
+
+//detalhes da vaga
+router.get("/view/:id", (req, res) => Job.findOne({
+  where: {
+    id: req.params.id
+  }
+}).then(job => {
+
+  res.render("view", {
+    job
+  })
+}).catch(err => {
+  console.log(err);
+}));
 
 //add job via post
 router.post("/add", (req, res) => {
